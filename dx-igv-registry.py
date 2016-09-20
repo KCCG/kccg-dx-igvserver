4c6a8ad387ce10f74493a7941512dead3963785b
Mark Cowley <m.cowley0@gmail.com>
2016-09-20 14:12:47 +1000

    update formatting, XML version 1, test project

--- dx-igv-registry.py
+++ dx-igv-registry.py
@@ -27,7 +27,7 @@ import dxpy
 
 class DxProjectRegistry(object):
     """Represent an DX Project as an IGV registry (XML)"""
-    
+
     def __init__(self, project, genome="1kg_v37", URL_DURATION=86400):
         """
         
@@ -36,20 +36,20 @@ class DxProjectRegistry(object):
         :param URL_DURATION: number of seconds for which the generated URL will be valid 
         """
         assert isinstance(project, dxpy.DXProject)
-        
+
         Global = Element('Global')
         Global.set("name", project.name)
-        Global.set("genome", genome)
-        Global.set("version", "3")
+        #Global.set("genome", genome)
+        Global.set("version", "1")
         self.Global = Global
         self.URL_DURATION = URL_DURATION
         self.genome = genome
         self.project = project
-    
+
     def addData(self):
         """Add all data within a DX project to this DxProjectRegistry instance, starting at top level"""
         self.addLevel(self.Global, "/")
-    
+
     def addLevel(self, node, folder):
         """
         Recurse into folders, and find all IGV-compatible files to be added to registry
@@ -60,18 +60,18 @@ class DxProjectRegistry(object):
         """
         assert node is not None
         assert folder is not None
-        
+
         print "Adding {}:{}".format(self.project.name, folder)
         subfolders = dxpy.api.project_list_folder(self.project.id, input_params={"folder": folder, "describe": {
             "fields": {"id": True, "name": True, "class": True}}, "only": "folders", "includeHidden": False},
                                                   always_retry=True)["folders"]
         subfolders = [os.path.basename(subfolder) for subfolder in subfolders]
-        
+
         for subfolder in subfolders:
             subnode = SubElement(self.Global, "Category", name=subfolder)
             subnodepath = str(folder + "/" + subfolder).replace("//", "/")
             self.addLevel(subnode, subnodepath)
-        
+
         dxfiles = list(dxpy.find_data_objects(
             recurse=False, folder=folder, return_handler=True, project=self.project.get_id())
         )
@@ -88,7 +88,7 @@ class DxProjectRegistry(object):
                     self.__addNonIndexedFile(dxfile, folder, node)
                 elif str(dxfile.name).endswith("tdf"):
                     self.__addNonIndexedFile(dxfile, folder, node)
-    
+
     def __addIndexedFile(self, dxfile, folder, node, index_exts=["bai"]):
         """
         Add a file to XML tree, which should also have an index file
@@ -100,11 +100,11 @@ class DxProjectRegistry(object):
         """
         print "Adding {}:{}/{}".format(self.project.name, folder, dxfile.name)
         assert isinstance(dxfile, dxpy.DXFile)
-        
+
         file_url = dxfile.get_download_url(
             duration=self.URL_DURATION, filename=dxfile.name, preauthenticated=True
         )
-        
+
         index = None
         for index_ext in index_exts:
             index_name = dxfile.name + "." + index_ext
@@ -116,20 +116,20 @@ class DxProjectRegistry(object):
             if not index is None:
                 break
         if index is None:
-            #raise dxpy.exceptions.DXSearchError("Could not find an index file for {}".format(dxfile.name))
-            #raise RuntimeWarning("Skipping {}, failed to find an index file".format(dxfile.name))
+            # raise dxpy.exceptions.DXSearchError("Could not find an index file for {}".format(dxfile.name))
+            # raise RuntimeWarning("Skipping {}, failed to find an index file".format(dxfile.name))
             print "Skipping {}, failed to find an index file".format(dxfile.name)
             return None
-        
+
         indel_url = index.get_download_url(
             duration=self.URL_DURATION, preauthenticated=True, filename=index.name
         )
-        
+
         resource = SubElement(node, "Resource")
         resource.set("name", dxfile.name)
         resource.set("path", file_url[0])
         resource.set("index", indel_url[0])
-    
+
     def __addNonIndexedFile(self, dxfile, folder, node):
         """
         Add a file to XML tree, by generating a DX URL
@@ -148,15 +148,15 @@ class DxProjectRegistry(object):
         resource = SubElement(node, "Resource")
         resource.set("name", dxfile.name)
         resource.set("path", file_url[0])
-    
+
     def getXmlName(self):
         filename = self.project.name + ".xml"
         return filename
-    
+
     def getRegistryName(self):
         filename = self.genome + "_dataServerRegistry.txt"
         return filename
-    
+
     def writeXML(self):
         """
         pretty print the XML tree.
@@ -165,39 +165,40 @@ class DxProjectRegistry(object):
         filename = self.getXmlName()
         rough_string = tostring(self.Global, 'utf-8', method="xml")
         reparsed = xml.dom.minidom.parseString(rough_string)
-        
+
         with open(filename, "w") as text_file:
             text_file.write(reparsed.toprettyxml(indent="\t", encoding='utf-8'))
-        
+
         # ElementTree(self.Global).write(filename, encoding="utf-8", xml_declaration=True)
         print "'%s' successfully created!" % filename
-    
+
     def writeRegistryTXT(self):
         filename = self.getRegistryName()
         with open(filename, "a") as myfile:
             url = quote("http://localhost:8000/igvdata/" + self.getXmlName(), safe="%/:=&?~#+!$,;'@()*[]")
             myfile.write(url + "\n")
-    
+
     def eraseRegistryTXT(self):
         filename = self.getRegistryName()
         if os.path.exists(filename):
             os.unlink(filename)
+
 
 class IgvRegistry(object):
     def __init__(self, root="/Users/marcow/igvdata", URL_DURATION=86400):
         self.root = root
         self.URL_DURATION = URL_DURATION
         self.txt = "1kg_v37_dataServerRegistry.txt"
-        
-        manifests=[]
+
+        manifests = []
         os.chdir(self.root)
         for file in glob.glob("*.xml"):
             manifests.append(str(file).replace(".xml", ""))
         self.projects = manifests
-    
+
     def getProjects(self):
         return self.projects
-    
+
     def newProjects(self):
         """
         Find projects available on DX, not present in local cache
@@ -209,7 +210,10 @@ class IgvRegistry(object):
         new_dx_projects = [x for x in new_dx_projects if not x.startswith('PIPELINE') and not x.endswith('resources')]
         print "Found {} new projects on DNAnexus, for {}".format(len(new_dx_projects), dxpy.whoami())
         return new_dx_projects
-    
+
+    def addProjectToCache(self, project):
+        self.projects.append(project)
+
     def updateCache(self):
         new_projects = self.newProjects()
         for project in new_projects:
@@ -217,8 +221,8 @@ class IgvRegistry(object):
             dxproj.addData()
             dxproj.writeXML()
             dxproj.writeRegistryTXT()
-            self.projects.append(project)
-    
+            self.addProjectToCache(project)
+
     def forceUpdate(self):
         os.chdir(self.root)
         for file in glob.glob("*.xml"):
@@ -226,15 +230,16 @@ class IgvRegistry(object):
         os.unlink(self.txt)
         self.projects = []
         self.updateCache()
-    
+
     def testUpdate(self):
         """Run a subset of projects"""
         first = True
-        #project_ids = (u'project-BzPb25j0627bFJv6q9g81ZX5', u'project-Bz6GbkQ0VGPv0fpqZZ6ZZGfx', u'project-Bb9KVk8029vp1qzXz4yx4xB3')
-        project_ids = (u'project-BzPb25j0627bFJv6q9g81ZX5', u'project-Bb9KVk8029vp1qzXz4yx4xB3')
+        # project_ids = (u'project-BzPb25j0627bFJv6q9g81ZX5', u'project-Bz6GbkQ0VGPv0fpqZZ6ZZGfx', u'project-Bb9KVk8029vp1qzXz4yx4xB3')
+        # project_ids = (u'project-BzPb25j0627bFJv6q9g81ZX5', u'project-Bb9KVk8029vp1qzXz4yx4xB3')
+        project_ids = (u'project-BzQ9qx80Y6qG6FF56Jq27145', u'project-BzPb25j0627bFJv6q9g81ZX5')  # NA12878 public
         for project_id in project_ids:
             project = dxpy.DXProject(project_id)
-            reg = DxProjectRegistry(project=project, genome="1kg_v37", URL_DURATION=60*24)
+            reg = DxProjectRegistry(project=project, genome="1kg_v37", URL_DURATION=ONE_MONTH)
             if first:
                 reg.eraseRegistryTXT()
                 first = False
@@ -242,16 +247,18 @@ class IgvRegistry(object):
             reg.writeXML()
             reg.writeRegistryTXT()
 
+
 # reg = IgvRegistry()
 # reg.getProjects()
 # reg.newProjects()
 # reg.testUpdate()
 
 ONE_HOUR = 3600
-ONE_DAY = 3600*24*1
-ONE_WEEK = 3600*24*7
-ONE_MONTH = 3600*24*31
-ONE_YEAR = 3600*24*365
+ONE_DAY = 3600 * 24 * 1
+ONE_WEEK = 3600 * 24 * 7
+ONE_MONTH = 3600 * 24 * 31
+ONE_YEAR = 3600 * 24 * 365
+
 
 def main(args):
     reg = IgvRegistry("/Users/marcow/igvdata", args.duration)
@@ -275,6 +282,7 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--test', help='Test mode, over a few projects only', action='store_true')
     parser.add_argument('-f', '--force', help='Force recreation of registry', action='store_true')
     parser.add_argument('-u', '--update', help='Update registry', action='store_true')
-    parser.add_argument('-d', '--duration', help='Duration to generate URLs for', type=int, required=False, default=ONE_MONTH, nargs=1)
+    parser.add_argument('-d', '--duration', help='Duration to generate URLs for', type=int, required=False,
+                        default=ONE_MONTH, nargs=1)
     args = parser.parse_args()
     main(args)
