d7aee401912f3dca447c923681c254cdc15af93e
Mark Cowley <marcow@lockdown.local>
2017-06-28 19:30:28 +1000

    update header

--- push2seave.sh
+++ push2seave.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# simple script to push the latest version of the app to seave.bio.
+# it's here becacuse you can't git pull the repo from outside GIMR w/out VPN.
+#
+# Mark Cowley
 server=ubuntu@seave.bio
 dest=/home/ubuntu/apps/kccg-dx-igvserver/
 #rsync -av virtualenv-1.11.6 requirements.txt dx-igv-registry.py $server:$dest
